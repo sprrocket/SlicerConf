6fba47bb2b1bd838adf05f668a3d559c1eed867f
Caleb Wellauer <caleb.wellauer@gmail.com>
2017-12-14 22:33:00 -0700

    init

--- SlicerConf/SlicerConf.Desktop.Main.cs
+++ SlicerConf/SlicerConf.Desktop.Main.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 namespace SlicerConf
 {
-    static class Program
+    static class MainProgram
     {
         /// <summary>
         /// The main entry point for the application.
@@ -16,7 +16,7 @@ namespace SlicerConf
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
--- SlicerConf/SlicerConf.Form.Main.Designer.cs
+++ SlicerConf/SlicerConf.Form.Main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SlicerConf
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
